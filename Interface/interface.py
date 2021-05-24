b5a3f7dd5a20ec8b8ca63e86d2e2e64081ce9fac
Domas112 <43904920+Domas112@users.noreply.github.com>
2021-05-24 14:05:24 +0300

    Updated Interface
    Updated prediction output format

--- Interface/interface.py
+++ Interface/interface.py
@@ -4,24 +4,22 @@ from PIL import Image
 from preprocess import preprocess
 from keras.models import load_model
 
-
-
 @st.cache
 def load_mdl():
     model = load_model("./Model/")
     return model
 
 def predict(model, image):
-    prediction = model.predict_classes(image)
+    image = preprocess(image)
+    prediction = model.predict(image)
     prediction = mean(prediction)
     return prediction
 
 
 model = load_mdl()
 
-st.title("Skin Myaloma Classification")
-st.write("Type of myaloma will be classified based on your uploaded image")
-image_file = st.file_uploader("Please upload the image here", type="jpg")
+st.title("Skin Myeloma Classification")
+image_file = st.file_uploader("Please upload the image here", type=["jpg", "png", "jpeg"])
 
 if(image_file is not None):
 
@@ -30,10 +28,15 @@ if(image_file is not None):
     st.image(img)
 
     if(st.button("Continue")):
-        preprocessed = preprocess(img)
-        prediction = predict(model, preprocessed)
+        prediction = predict(model, img)
+        st.write("The type of myaloma is predicted to be ")
         
-        st.text(prediction)
+        if(prediction):
+            st.header("Malignant")
+        
+        else:
+            st.header("Benign")
+            
 
 
 
